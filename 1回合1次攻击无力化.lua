63616c8e7faa1d58ce23edaff4a350f0ac894ff9
resparkstar <39150614+resparkstar@users.noreply.github.com>
2020-03-10 23:30:18 +0800

    Add files via upload

---
+++ 1回合1次攻击无力化.lua
@@ -0,0 +1,31 @@
+-- 轮回之珀耳修斯
+function c42444868.initial_effect(c)
+	 --Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(c42444868.condition)
+	e1:SetTarget(c42444868.target)
+	e1:SetOperation(c42444868.activate)
+	c:RegisterEffect(e1)
+end
+function c42444868.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer()
+end
+function c42444868.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local tg=Duel.GetAttacker()
+	if chkc then return chkc==tg end
+	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(tg)
+end
+function c42444868.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE,1)  
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsCanTurnSet() then
+		Duel.BreakEffect()
+		c:CancelToGrave()
+		Duel.ChangePosition(c,POS_FACEDOWN)
+		Duel.RaiseEvent(c,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
+	end
+end
